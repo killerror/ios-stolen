41ce8d81f6d56bb968798f6c921706e39f07fbb3
Максим Бобков <biramax@BIRAMAX-MACBOOK.local>
2024-01-04 22:53:59 +0300

    Created Storyboard project on Seminar #1

---
+++ Seminar1. Storyboard/MyProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyProject
+//
+//  Created by Максим Бобков on 04.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
